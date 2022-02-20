210fb5caa289605a0da12551aa1ca1e020518075
mohanabu <mohanabu1331@gmail.com>
2022-02-20 09:31:14 -0800

    bank

---
+++ src/org/hit/Task1.java
@@ -0,0 +1,17 @@
+package org.hit;
+
+public class Task1 {
+	private void mohan() {
+		System.out.println("mohan");
+
+	}
+private void manoj() {
+	System.out.println("manoj ");
+
+}
+public static void main(String[] args) {
+	Task1 task  = new Task1();
+	task.manoj();
+	task.mohan();
+}
+}
